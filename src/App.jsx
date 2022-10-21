013ef08d37afa7db3f519248180be0ceb80bb043
michael baker <56899037+PxPerfectMike@users.noreply.github.com>
2022-10-20 23:59:39 -0700

    impliment styles, name, and nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,28 @@ import React, { useState, useEffect } from 'react';
 function App() {
 	return (
 		<>
-			<div className='App'></div>
+			<div className='App'>
+				<div className='name'>
+					<h1>Michael Baker</h1>
+					<p>Front-End Developer</p>
+				</div>
+				<div className='nav'>
+					<ul>
+						<li>
+							<a href='#'>Home</a>{' '}
+						</li>
+						<li>
+							<a href='#'>Projects</a>{' '}
+						</li>
+						<li>
+							<a href='#'>Info</a>
+						</li>
+						<li>
+							<a href='#'>Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</>
 	);
 }
